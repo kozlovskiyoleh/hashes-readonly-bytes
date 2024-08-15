1aeed52634aff94102045a735f5d737df33d7846
Oleh Kozlovskiy <oleg.kozlovskiy.35p@gmail.com>
2024-08-15 19:56:25 +0300

    All tests are passed

--- ReadonlyBytes.cs
+++ ReadonlyBytes.cs
@@ -10,8 +10,9 @@ namespace hashes
 	public class ReadonlyBytes : IEnumerable<byte>
 	{
 		private readonly byte[] array;
-
+        public int Hash { get; private set; }
 		public int Length => array.Length;
+        private bool isCalculate = false;
 
 		public byte this[int index]
 		{
@@ -34,11 +35,38 @@ namespace hashes
 			if (data == null)
 				throw new ArgumentNullException(nameof(data));
 			array = data;
+            Hash = GetHashCode();
 		}
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj.GetType() != typeof(ReadonlyBytes))
+                return false;
+            ReadonlyBytes other = obj as ReadonlyBytes;
+            return other.Hash == Hash;
+        }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(array);
+            ulong offset_basis = 14695981039346656037;
+            ulong FNV_prime = 1099511628211;
+            ulong hash = offset_basis;
+            unchecked
+            {
+                if (isCalculate)
+                    return Hash;
+                else 
+                {
+                    foreach (var item in array)
+                    {
+                        hash ^= item;
+                        hash *= FNV_prime;
+                    }
+                }
+                isCalculate = true;
+                Hash = (int)hash;
+                return Hash;
+            }
         }
 
         public override string ToString()
